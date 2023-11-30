56147d3eb8899a76f8243dad6c8228b5f3771384
morozgit <morozec76@gmail.com>
2023-11-30 16:42:56 +0300

    setup DialogFlow

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from google.cloud import dialogflow
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -23,9 +24,22 @@ def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+def detect_intent_texts(update: Update, context: CallbackContext):
+    project_id = 'recognizingspeech'
+    session_id = 'temchmorozov'
+    language_code = 'ru'
+    texts = update.message.text
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
 
+    # for text in texts:
+    text_input = dialogflow.TextInput(text=texts, language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    update.message.reply_text(response.query_result.fulfillment_text)
 
 def main():
     load_dotenv(find_dotenv())
@@ -33,8 +47,8 @@ def main():
     updater = Updater(tg_token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, detect_intent_texts))
+
     updater.start_polling()
     updater.idle()
 
