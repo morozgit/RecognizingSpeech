cbc3b8670581c1efed0552173d462231450368d1
morozgit <morozec76@gmail.com>
2023-12-05 16:04:10 +0300

    Update vk_bot.py

--- vk_bot.py
+++ vk_bot.py
@@ -1,15 +1,32 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+import os
+from dotenv import load_dotenv, find_dotenv
 
-vk_session = vk_api.VkApi(token="vk1.a.zZEdRdlMs2gzzDGq6NhF3_Twyq8ukpuRi4MJ0F9yADTHtzNkr8r6n9g3GRrAI00y83RoJhfUG2yh0KlUcs0BCbYHVho3SbvgYBp8LpWy7FR4EugWHaSPlnEpfRABjpK6WrJxQMKnvGaW2Dgy9WwgkGoA7jDjb2WGbifh_pbrMJk5z7EdoBf55KDTDOWj6rn41gCGZbg64mRUJloNJ7h_-w")
 
-longpoll = VkLongPoll(vk_session)
+def discussing_with_vk(vk_token):
+    vk_session = vk_api.VkApi(token=vk_token)
+    longpoll = VkLongPoll(vk_session)
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        print('Новое сообщение:')
-        if event.to_me:
-            print('Для меня от: ', event.user_id)
-        else:
-            print('От меня для: ', event.user_id)
-        print('Текст:', event.text)
+    try:
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW:
+                print('Новое сообщение:')
+                if event.to_me:
+                    print('Для меня от: ', event.user_id)
+                else:
+                    print('От меня для: ', event.user_id)
+                print('Текст:', event.text)
+
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
+
+def main():
+    load_dotenv(find_dotenv())
+    vk_token = os.environ.get("VK_TOKEN")
+    discussing_with_vk(vk_token)
+
+
+if __name__ == '__main__':
+    main()
