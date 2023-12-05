93c9419b275b33a7a58a5c100e3a0c3672b6fbb0
morozgit <morozec76@gmail.com>
2023-12-05 16:20:52 +0300

    add DialogFlow to VK bot.

--- vk_bot.py
+++ vk_bot.py
@@ -4,12 +4,34 @@ import os
 from dotenv import load_dotenv, find_dotenv
 import random
 import vk_api as vk
+from google.cloud import dialogflow
 
 def echo(event, vk_api):
     vk_api.messages.send(
         user_id=event.user_id,
         message=event.text,
         random_id=random.randint(1,1000)
+    )
+
+def detect_intent_texts(event, vk_api):
+    project_id = 'recognizingspeech'
+    session_id = 'temchmorozov'
+    language_code = 'ru'
+    texts = event.text
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    # for text in texts:
+    text_input = dialogflow.TextInput(text=texts, language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=response.query_result.fulfillment_text,
+        random_id=random.randint(1, 1000)
     )
 
 
@@ -22,6 +44,7 @@ def discussing_with_vk(vk_token):
         for event in longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                 echo(event, vk_api)
+                detect_intent_texts(event, vk_api)
                 print('Новое сообщение:')
                 if event.to_me:
                     print('Для меня от: ', event.user_id)
